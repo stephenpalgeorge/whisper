ae1029584620835f96dde0591501d195c30e1cda
Stephen Pal-George <stephenpalgeorge@gmail.com>
2022-07-05 14:34:38 +0100

    setup sass and pick some colors

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -8,3 +8,18 @@
     </header>
     <slot></slot>
 </main>
+
+<style lang="scss">
+    @use '../lib/styles/variables' as var;
+
+    :global{
+        h1, h2, h3, h4, h5, h6,
+        p, label, button {
+          color: var.$clr--eight-ball;
+        }
+
+        a {
+          color: var.$clr--deep-larkspur;
+        }
+    }
+</style>
